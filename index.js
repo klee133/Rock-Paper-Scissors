d88b06a4d72a65560172a8cd8ae5fd68ca90e42e
klee133 <61660035+klee133@users.noreply.github.com>
2025-06-25 13:57:27 -0400

    Create getComputerChoice() function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getComputerChoice() {
+    let choice = Math.floor((Math.random() * 10)) % 3;
+
+    switch(choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+    }
+    return "scissors";
+}
+
+console.log(getComputerChoice());
 
