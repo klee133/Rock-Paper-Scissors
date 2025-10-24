61629cb152c9e89f373e2c2c363154b83f7193f8
klee133 <61660035+klee133@users.noreply.github.com>
2025-10-24 15:25:55 -0400

    Refactor code to have 5 rounds per game

--- index.js
+++ index.js
@@ -9,69 +9,74 @@ function getComputerChoice() {
     }
     return "scissors";
 }
-// console.log(getComputerChoice());
-// console.log(getHumanChoice());
 
-function playGame(humanChoice) {
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        if(humanChoice === computerChoice) {
-            console.log("You tied! You each get a point.");
-            humanScore++;
-            computerScore++;
-        }
-        
-        if(humanChoice === "rock") {
-            if(computerChoice === "paper") {
-                console.log("You lose! Paper beats rock.");
-                computerScore++;
-            }else if(computerChoice === "scissors") {
-                console.log("You win! Rock beats scissors.");
-                humanScore++;
-            }
-        }else if(humanChoice === "paper") {
-            if(computerChoice === "rock") {
-                console.log("You win! Paper beats rock.");
-                humanScore++;
-            }else if(computerChoice === "scissors") {
-                console.log("You lose! Scissors beats paper.");
-                computerScore++;
-            }
-        }else if(humanChoice === "scissors") {
-            if(computerChoice === "paper") {
-                console.log("You win! Scissors beats paper.");
-                humanScore++;
-            }else if(computerChoice === "rock") {
-                console.log("You lose! Rock beats scissors.");
-                computerScore++;
-            }
-        }
-    }
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
 
-    let i = 0;
-    console.log("Round " + (i+1));
-    const humanSelection = humanChoice;
-    const computerSelection = getComputerChoice();
-    console.log("Human: " + humanSelection + ", Computer: " + computerSelection)
-    playRound(humanSelection, computerSelection);
-
+function endGame() {
     console.log("Your score: " + humanScore);
     console.log("Computer's score: " + computerScore);
-
-    if(humanScore == computerScore) {
-        console.log("You tied with the computer!");
-    }else if(humanScore > computerScore) {
-        console.log("You won!");
+    if(round >= 5) {
+        if(humanScore == computerScore) {
+            console.log("You tied with the computer!");
+        }else if(humanScore > computerScore) {
+            console.log("You won!");
+        }else{
+            console.log("You lost!");
+        }
+        humanScore = 0;
+        computerScore = 0;
+        round = 1;
     }else{
-        console.log("You lost!");
+        round++;
     }
+}
+
+function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
+
+    console.log("Round " + round);
+    console.log("Human: " + humanChoice + ", Computer: " + computerChoice)
+
+    if(humanChoice === computerChoice) {
+        console.log("You tied! You each get a point.");
+        humanScore++;
+        computerScore++;
+    }
+    
+    if(humanChoice === "rock") {
+        if(computerChoice === "paper") {
+            console.log("You lose! Paper beats rock.");
+            computerScore++;
+        }else if(computerChoice === "scissors") {
+            console.log("You win! Rock beats scissors.");
+            humanScore++;
+        }
+    }else if(humanChoice === "paper") {
+        if(computerChoice === "rock") {
+            console.log("You win! Paper beats rock.");
+            humanScore++;
+        }else if(computerChoice === "scissors") {
+            console.log("You lose! Scissors beats paper.");
+            computerScore++;
+        }
+    }else if(humanChoice === "scissors") {
+        if(computerChoice === "paper") {
+            console.log("You win! Scissors beats paper.");
+            humanScore++;
+        }else if(computerChoice === "rock") {
+            console.log("You lose! Rock beats scissors.");
+            computerScore++;
+        }
+    }
+    endGame();
 }
 
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        playGame(button.id);
+        playRound(button.id);
     });
 });
